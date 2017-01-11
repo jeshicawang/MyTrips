5542a49363695d7a635f863e2afbf4bbc947533c
jeshicawang <jeshicawang@gmail.com>
2017-01-11 15:38:04 -0800

    Added GET routes to retrieve data from trips & destinations tables. #5

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
 
+const knex = require('knex')({
+  client: 'postgresql',
+  connection: {
+    user: 'mytrips',
+    database: 'mytrips'
+  }
+});
+
 app.use(express.static('public'));
+
+app.get('/trips/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const query = knex('trips')
+    .where('user_id', userId)
+    .select('id', 'title', 'description', 'start_date', 'end_date', 'notes');
+  query.then(trips => res.json(trips));
+});
+
+app.get('/destinations/:tripId', (req, res) => {
+  const tripId = req.params.tripId;
+  const query = knex('destinations')
+    .where('trip_id', tripId)
+    .select('id', 'location', 'place_id', 'start_date', 'end_date');
+  query.then(destinations => res.json(destinations));
+});
 
 app.listen(3000, () => console.log('listening on port 3000'));
