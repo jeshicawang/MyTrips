f418be4407d36eb389d8d27fa980ae9ede6e7902
jeshicawang <jeshicawang@gmail.com>
2017-01-11 15:14:52 -0800

    Created database and tables and inserted sample trip information. #5

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+exports.development = {
+  client: 'postgresql',
+  connection: {
+    user: 'mytrips',
+    database: 'mytrips'
+  },
+  migrations: {
+    directory: './migrations',
+    tableName: 'knex_migrations'
+  }
+}
