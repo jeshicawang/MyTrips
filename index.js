c21db3f3e819a92ffecf04985044d98618e19299
jeshicawang <jeshicawang@gmail.com>
2017-01-12 13:43:41 -0800

    Display trips using information fetched from the database only. #5

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ app.use(express.static('public'));
 
 app.get('/trips/:userId', (req, res) => {
   const userId = req.params.userId;
+  const dateFormat = 'Dy, Month DD';
+  const formattedStartDate = knex.raw('to_char(start_date, \'' + dateFormat + '\') as start_date');
+  const formattedEndDate = knex.raw('to_char(end_date, \'' + dateFormat + '\') as end_date')
   const query = knex('trips')
     .where('user_id', userId)
-    .select('id', 'title', 'description', 'start_date', 'end_date', 'notes');
+    .select('id', 'title', 'description', formattedStartDate, formattedEndDate, 'notes');
   query.then(trips => res.json(trips));
 });
 
@@ -23,7 +26,7 @@ app.get('/destinations/:tripId', (req, res) => {
   const tripId = req.params.tripId;
   const query = knex('destinations')
     .where('trip_id', tripId)
-    .select('id', 'location', 'place_id', 'start_date', 'end_date');
+    .select('id', 'trip_id', 'location', 'place_id', 'start_date', 'end_date', 'photo_url');
   query.then(destinations => res.json(destinations));
 });
 
--- public/main.js
+++ public/main.js
@@ -3,37 +3,37 @@ const currentUser = 2;
 const tripsContainer = document.getElementById('trips');
 
 fetch('/trips/' + currentUser)
-  .then((results) => results.json())
-  .then((results) => {
-    results.map(({ id, title, description, start_date, end_date, notes }) => {
-      const tripElement = createElement('div', { id: 'trip' + id, class: 'trip' },
-                            createElement('div', { class: 'layer' }, [
-                              createElement('h2', { class: 'title' }, title),
-                              createElement('p', { class: 'description' }, description ? description : 'no description provided'),
-                              createElement('p', { class: 'date' }, start_date + ' - ' + end_date)]))
-      fetch('/destinations/' + id)
-        .then((results) => results.json())
-        .then(([firstResult]) => {
-          const placeId = firstResult.place_id;
-          const request = { placeId: placeId };
-          const service = new google.maps.places.PlacesService(new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -33.8666, lng: 151.1958},
-            zoom: 15
-          }));
-          service.getDetails(request, (place, status) => {
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-              const photoUrl = place.photos[0].getUrl({'maxWidth': 1600});
-              tripElement.style.backgroundImage = 'url(' + photoUrl + ')';
-            }
-          });
-        })
-        .catch((error) => console.error(error));
-      return tripElement;
-    }).forEach(tripElement => {
-      tripsContainer.appendChild(tripElement);
-    });
-  })
-  .catch((error) => console.error(error));
+  .then(convertToObject)
+  .then(displayTrips)
+  .catch(logError);
+
+function convertToObject(results) {
+  return results.json();
+}
+
+function displayTrips(results) {
+  results.map(({id, title, description, start_date, end_date, notes}) =>
+    createElement('div', { id: id, class: 'trip' },
+      createElement('div', { class: 'layer' }, [
+        createElement('h2', { class: 'title' }, title),
+        createElement('p', { class: 'description' }, description ? description : 'no description provided'),
+        createElement('p', { class: 'date' }, start_date + ' - ' + end_date)]))
+  ).forEach(tripElement => {
+    tripsContainer.appendChild(tripElement);
+    fetch('/destinations/' + tripElement.id)
+      .then(convertToObject)
+      .then(setDestinationPhoto)
+      .catch(logError);
+  });
+}
+
+function setDestinationPhoto([{trip_id, photo_url}]) {
+  document.getElementById(trip_id).style.backgroundImage = 'url(' + photo_url + ')';
+}
+
+function logError(error) {
+  return console.error(error);
+}
 
 // Returns a new element w/ the given tag, attrubutes, children, & eventListener
 function createElement(tag, attributes, children, eventListener) {
