86d45e9152fd408b19a5c1f218e49d76d20663ef
jeshicawang <jeshicawang@gmail.com>
2017-01-12 20:58:57 -0800

    Modified stylesheet and added input field with autocomplete. #1

--- public/main.js
+++ public/main.js
@@ -79,3 +79,34 @@ function createElement(tag, attributes, children) {
     return element;
   }, newElement);
 }
+
+let autocomplete;
+
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+            {types: ['(cities)']}
+  );
+  autocomplete.addListener('place_changed', createNewTrip)
+}
+
+function createNewTrip() {
+  const place = autocomplete.getPlace();
+  console.log(place);
+}
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
