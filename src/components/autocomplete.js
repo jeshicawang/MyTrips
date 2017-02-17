496abf9c6208f313bad46b36607ad63a52d2934c
jeshicawang <jeshicawang@gmail.com>
2017-02-16 16:26:48 -0800

    Fixing bug. #20

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -1,16 +1,11 @@
 /* global google */
 const React = require('react');
 
-const Autocomplete = ({ index, value, placeholder, autocomplete, handlePlaceChange, saveAutocomplete, handleChange }) => {
+const Autocomplete = ({ value, placeholder, autocomplete, handlePlaceChange, saveAutocomplete, handleChange }) => {
 
   const initAutocomplete = (element) => {
     // return if the input is merely being updated and an autocomplete already exists
     if (autocomplete || !element) return;
-
-    // Removing google generated .pac-container if necessary
-    const containerIndex = index ? index : 0
-    const container = document.getElementsByClassName('pac-container')[containerIndex];
-    if (container) container.parentElement.removeChild(container);
 
     // Creating a new google autocomplete object
     const newAutocomplete = new google.maps.places.Autocomplete(
@@ -29,7 +24,6 @@ const Autocomplete = ({ index, value, placeholder, autocomplete, handlePlaceChan
 }
 
 Autocomplete.propTypes = {
-  index: React.PropTypes.number,
   value: React.PropTypes.string,
   placeholder: React.PropTypes.string.isRequired,
   autocomplete: React.PropTypes.object,
