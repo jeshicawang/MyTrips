4e190937659c727fd92e3bf7f17568d304040a24
jeshicawang <jeshicawang@gmail.com>
2017-02-17 11:50:41 -0800

    Adding error-handling middleware. #20

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const app = express();
 const bodyParser = require('body-parser')
 const usersRoutes = require('./users-routes.js');
 const tripsRoutes = require('./trips-routes.js');
+const errorHandler = require('./error-handler');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use('/users', usersRoutes());
 app.use('/trips', tripsRoutes());
+app.use(errorHandler);
 
 module.exports = app;
