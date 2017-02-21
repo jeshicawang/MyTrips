a111c118307c0d11ae853790741e3743082275d5
jeshicawang <jeshicawang@gmail.com>
2017-02-21 14:56:41 -0800

    Using NODE_ENV variable. #22

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,12 @@ module.exports = {
       directory: './migrations',
       tableName: 'knex_migrations'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    }
   }
 }
