c981e494ca9b7ea5ae603fd5a6ce674a531bbe66
jeshicawang <jeshicawang@gmail.com>
2017-02-22 22:07:33 -0800

    REmoving migrations property from production in knexfile. #24

--- knexfile.js
+++ knexfile.js
@@ -19,9 +19,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './migrations'
-    }
+    connection: process.env.DATABASE_URL
   }
 }
